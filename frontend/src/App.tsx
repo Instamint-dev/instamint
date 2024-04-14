5d555bc888067f018de6ab3935061ab9b1aef448
antomth971 <anthonymathieu21@live.fr>
2024-04-14 15:16:03 +0200

    fix after review

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import RegisterPage from "./feature/register/Register.tsx"
 import HomePage from "./feature/Home.tsx"
 import ConnectionPage from "./feature/connection/Connection.tsx"
 import { AuthProvider } from "./providers/AuthProvider.tsx"
-import ForgotPasswordPage from "./feature/mailToken/forgotPassword/ForgotPassword.tsx"
+import ForgotPassword from "./feature/mailToken/forgotPassword/ForgotPassword.tsx"
 import GeneratePassword from "./feature/mailToken/forgotPassword/GeneratePassword.tsx"
 import RegisterToken from "./feature/mailToken/registerToken/registerToken.tsx"
 
@@ -15,7 +15,7 @@ function App() {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/connection" element={<ConnectionPage />} />
                     <Route path="/register" element={<RegisterPage/>} />
-                    <Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
+                    <Route path="/forgot-password" element={<ForgotPassword/>}/>
                     <Route path="/generate-password/:id" element={<GeneratePassword/>}/>
                     <Route path="/register_token/:id" element={<RegisterToken/>}/>
                 </Routes>
