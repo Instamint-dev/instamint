644f30d58188ba5f4acde1211527312bcb2bfff6
antomth971 <anthonymathieu21@live.fr>
2024-04-19 20:05:15 +0200

    add redirect + fix .env backendand frontend .env linter + delete console.error

--- frontend/src/feature/PageNotFound.tsx
+++ frontend/src/feature/PageNotFound.tsx
@@ -1,14 +1,13 @@
 import Navbar from "./navbar/navbar"
 
-const PageNotFound = () => {
-    return (
-        <> <Navbar />
-            <div className="flex items-center flex-col">
-                <h1>404 not found</h1>
-                <p>The requested page could not be found.</p>
-            </div>
-        </>
-    )
-}
+const PageNotFound = () => (
+    <>
+        <Navbar />
+        <div className="flex items-center flex-col">
+            <h1>404 not found</h1>
+            <p>The requested page could not be found.</p>
+        </div>
+    </>
+)
 
 export default PageNotFound
