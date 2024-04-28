f7dfb833fcf381f43954abe027497f5a765976fe
metrike <kevinmetri.pro@gmail>
2024-04-28 18:27:24 +0200

    beug button front

--- frontend/src/feature/NFT/PostNFT/NFTPost.tsx
+++ frontend/src/feature/NFT/PostNFT/NFTPost.tsx
@@ -69,13 +69,13 @@ const NFTPost = () => {
                             className="object-cover w-full h-full"/>
                        <div className="absolute bottom-2 right-2 space-x-2">
                        </div>
-                       <div className="absolute bottom-2 right-2 space-x-2">
-                           <button onClick={() => handleCopyLink(image.id)} className="bg-black hover:bg-blue-600 text-white font-bold py-1 px-2 rounded z-10">
+                       <div className="absolute bottom-2 right-2 space-x-2 z-10">
+                           <button onClick={() => handleCopyLink(image.id)} className="bg-black hover:bg-blue-600 text-white font-bold py-1 px-2 rounded">
                                <svg className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"/>
                                </svg>
                            </button>
-                           <button onClick={() => handleModalOpen(image.id)} className="bg-green-500 hover:bg-blue-600 text-white font-bold py-1 px-2 rounded z-10">
+                           <button onClick={() => handleModalOpen(image.id)} className="bg-green-500 hover:bg-blue-600 text-white font-bold py-1 px-2 rounded">
                                <svg className="h-8 w-8 text-blue-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                    <line x1="22" y1="2" x2="11" y2="13" />
                                    <polygon points="22 2 15 22 11 13 2 9 22 2" />
