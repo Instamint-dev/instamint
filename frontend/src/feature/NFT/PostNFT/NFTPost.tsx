5eeeeab51375c033a79a9b118292d8e44fdde881
metrike <kevinmetri.pro@gmail>
2024-04-28 18:24:11 +0200

    beug button front

--- frontend/src/feature/NFT/PostNFT/NFTPost.tsx
+++ frontend/src/feature/NFT/PostNFT/NFTPost.tsx
@@ -69,8 +69,8 @@ const NFTPost = () => {
                             className="object-cover w-full h-full"/>
                        <div className="absolute bottom-2 right-2 space-x-2">
                        </div>
-                       <div className="absolute bottom-2 right-2 space-x-2 z-10">
-                           <button onClick={() => handleCopyLink(image.id)} className="bg-black hover:bg-blue-600 text-white font-bold py-1 px-2 rounded">
+                       <div className="absolute bottom-2 right-2 space-x-2">
+                           <button onClick={() => handleCopyLink(image.id)} className="bg-black hover:bg-blue-600 text-white font-bold py-1 px-2 rounded z-10">
                                <svg className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"/>
                                </svg>
