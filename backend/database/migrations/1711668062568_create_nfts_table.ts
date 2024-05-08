02f27e357c3e4ed90afef4259e89268e62cb1d81
metrike <kevinmetri.pro@gmail>
2024-05-09 01:55:21 +0200

    add NFT in seeders and change icon to like

--- backend/database/migrations/1711668062568_create_nfts_table.ts
+++ backend/database/migrations/1711668062568_create_nfts_table.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('link', 100)
+      table.string('link', 100).unique()
       table.string('description', 100)
       table.string('hashtags', 100)
       table.string('place', 100)
--- frontend/src/feature/NFT/PostNFT/ComponentPublicationNFT/Like.tsx
+++ frontend/src/feature/NFT/PostNFT/ComponentPublicationNFT/Like.tsx
@@ -1,17 +1,7 @@
 const Like = ({ onClick,numberOfLike }: { onClick: () => void ,numberOfLike:number|undefined}) => (
     <button className="flex items-center focus:outline-none" onClick={onClick}>
-        <svg
-            className="h-8 w-8 text-red-500"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            stroke="none"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
-        </svg>
-        <span className="text-sm text-red-500 ml-2">{numberOfLike}</span>
+        <img src="https://instamintkami.blob.core.windows.net/instamint/Like.svg" alt="Like" className="h-8 w-8 text-black-500" />
+        <span className="text-sm text-green-600 ml-2">{numberOfLike}</span>
     </button>
 )
 
