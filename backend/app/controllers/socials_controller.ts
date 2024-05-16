35a307f83079bd8420b5593acd9904fdcd07b0a0
metrike <kevinmetri.pro@gmail>
2024-05-16 23:36:46 +0200

    fix review

--- backend/app/controllers/socials_controller.ts
+++ backend/app/controllers/socials_controller.ts
@@ -329,24 +329,6 @@ export default class SocialsController {
         )
 
         return response.status(200).json({ return: SocialsController.QUIT_TEA_BAG })
-
-      case SocialsController.COOK_EXIT_TEA_BAG:
-        const user = await auth.use('api').user
-        if (!user) {
-          return response.status(200).json({ return: 0 })
-        }
-
-        await db
-          .from('followers')
-          .where('follower', user.id)
-          .andWhere('followed', USER_EXIST.id)
-          .delete()
-
-        await db
-          .from('tea_bags')
-          .where('id', USER_EXIST.id)
-          .update('cook', db.raw('JSON_REMOVE(cook, ?)', [JSON.stringify(user.id)]))
-        this.deleteNotification(USER_EXIST, user, 3)
     }
   }
   protected async isFollowPrivate({ request, response, auth }: HttpContext) {
