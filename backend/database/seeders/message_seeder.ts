e4a9ce7e1de49ff6a960351420263afcd1d8ed9f
metrike <kevinmetri.pro@gmail>
2024-05-19 18:50:02 +0200

    create new message

--- backend/database/seeders/message_seeder.ts
+++ backend/database/seeders/message_seeder.ts
@@ -8,44 +8,43 @@ export default class MessageSeeder extends BaseSeeder {
                 sender_id: 1,
                 receiver_id: 2,
                 content: "Il fait beau aujourd'hui !",
-                send_date: '2024-05-04', // Définir la date au format YYYY-MM-DD
+                send_date: '2024-05-04',
             },
             {
                 sender_id: 2,
                 receiver_id: 1,
                 content: "Oui, c'est une journée magnifique !",
-                send_date: '2024-05-05', // Définir la date au format YYYY-MM-DD
+                send_date: '2024-05-05',
             },
             {
                 sender_id: 1,
                 receiver_id: 2,
                 content: "Hier, le soleil brillait fort !",
-                send_date: '2024-05-06', // Définir la date au format YYYY-MM-DD
+                send_date: '2024-05-06',
             },
             {
                 sender_id: 2,
                 receiver_id: 1,
                 content: "Je suis d'accord, c'était une journée parfaite !",
-                send_date: '2024-05-07', // Définir la date au format YYYY-MM-DD
+                send_date: '2024-05-07',
             },
-            // Ajout d'une discussion entre les utilisateurs 2 et 4
             {
                 sender_id: 2,
                 receiver_id: 4,
                 content: "Salut, comment ça va ?",
-                send_date: '2024-05-06', // Définir la date au format YYYY-MM-DD
+                send_date: '2024-05-06',
             },
             {
                 sender_id: 4,
                 receiver_id: 2,
                 content: "Ça va bien, merci ! Et toi ?",
-                send_date: '2024-05-06', // Définir la date au format YYYY-MM-DD
+                send_date: '2024-05-06',
             },
             {
                 sender_id: 4,
                 receiver_id: 2,
                 content: "Je vais bien aussi, merci !",
-                send_date: '2024-05-07', // Définir la date au format YYYY-MM-DD
+                send_date: '2024-05-07',
             },
         ])
     }
