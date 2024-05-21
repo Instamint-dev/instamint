11da8a5556da4b4f8721bb73a10d03e9bb5fd443
metrike <kevinmetri.pro@gmail>
2024-05-21 15:05:43 +0200

    fix linter

--- frontend/src/type/feature/PrivateMessaging/ResponsePreviewMessage.ts
+++ frontend/src/type/feature/PrivateMessaging/ResponsePreviewMessage.ts
@@ -1,12 +1,12 @@
 interface ResponsePreviewMessage{
-    id: number,
-    otherUsername: string,
-    otherId: number,
-    otherImage: string,
-    content: string,
-    sendDate: string,
-    senderId: number,
-    read: boolean,
+    id: number
+    otherUsername: string
+    otherId: number
+    otherImage: string
+    content: string
+    sendDate: string
+    senderId: number
+    read: boolean
 }
 
 export default ResponsePreviewMessage
