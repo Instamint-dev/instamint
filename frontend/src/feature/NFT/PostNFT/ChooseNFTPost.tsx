f7ea52e1f082fe66d91d05c5f2975416b0d36c6c
metrike <kevinmetri.pro@gmail>
2024-05-29 15:59:40 +0200

    add button to return to drafts

--- frontend/src/feature/NFT/PostNFT/ChooseNFTPost.tsx
+++ frontend/src/feature/NFT/PostNFT/ChooseNFTPost.tsx
@@ -5,6 +5,7 @@ import {useTranslation} from "react-i18next"
 import Navbar from "../../navbar/navbar.tsx"
 import {getDraftsCompleted} from "./service/PostNFTService.ts"
 import {Link} from "react-router-dom"
+import CustomButton from "../../../components/CustomButton.tsx";
 
 
 const ChooseNFTPost = () => {
@@ -36,6 +37,11 @@ const ChooseNFTPost = () => {
             <div className="flex flex-col items-center min-h-screen">
                 <h2 className="text-2xl font-bold text-gray-800 mt-4 mb-2">
                     {t("Choose NFT to Post (Draft must be completed)")}
+                    <div className={"flex justify-center"}>
+                        <Link to={"/nft/"}>
+                                <CustomButton  value={t("Drafts")} type="submit"/>
+                        </Link>
+                    </div>
                 </h2>
                 <div className="grid grid-cols-3 gap-4 mt-4">
             {images.map(image => (
